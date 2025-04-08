f28978449a24753f9914cb746ab3c2d1911c212d
Subham Padhi <isubhampadhi56@gmail.com>
2025-04-08 10:00:34 +0530

    add typeorm db in packages

---
+++ packages/database/src/entities/users.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User{
+    @PrimaryGeneratedColumn()
+    id?: number
+
+    @Column()
+    firstName!: string
+
+    @Column()
+    lastname?: string
+
+    @Column()
+    username!: string
+
+    @Column()
+    password!: string
+
+    @Column()
+    email!: string
+
+    @Column()
+    role!: string
+}
---
+++ packages/database/src/index.ts
@@ -0,0 +1,42 @@
+import { SqliteConnectionOptions } from "typeorm/driver/sqlite/SqliteConnectionOptions.js";
+import { User } from "./entities/users.entity";
+import { DataSource } from "typeorm";
+
+const drawEntities = [
+    User
+]
+const sqlitedbConnectionOption: SqliteConnectionOptions = {
+    type: "sqlite",
+    database: "draw.sqlite",
+    entities: drawEntities,
+    logging: true,
+    synchronize: true
+}
+
+export const drawAppDataSource = new DataSource(sqlitedbConnectionOption);
+
+export async function connectDB() {
+    try{
+        await drawAppDataSource.initialize();
+    }catch(err){
+        console.error(err);
+    }
+}
+
+export async function clearDB(){
+    try{
+        await drawAppDataSource.synchronize(true);
+    }catch(err){
+        console.error(err);
+    }
+}
+
+export async function disconnectDB(){
+    try{
+        await drawAppDataSource.destroy();
+    }catch(err){
+        console.error(err);
+    }
+}
+
+export const userRepo = drawAppDataSource.getRepository(User)
